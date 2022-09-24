8b2fb8be692a6ba1e46bc51982f85e986c140e73
Jess Moore <jessclaremoore@gmail.com>
2022-09-24 19:10:35 +1000

    corrected icon colour

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,10 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Add counter',
-        child: const Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
